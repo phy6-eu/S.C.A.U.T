eac671baf3dd7b1482b7e1c8ed0e13dd0c109f12
Julian Göpel <julian.goepel@izfp.fraunhofer.de>
2026-06-11 15:29:13 +0200

    Fixes by help of AI

--- main.cpp
+++ main.cpp
@@ -111,15 +111,22 @@ void createOutOfFile(){
 		return;
 	}
 	
-	ImportFile.seekg (0, ImportFile.end);
-    	int length = ImportFile.tellg();
-    	ImportFile.seekg (0, ImportFile.beg);
-	char* buffer = new char[length];
+//	ImportFile.seekg (0, ImportFile.end);
+//    	int length = ImportFile.tellg();
+//    	ImportFile.seekg (0, ImportFile.beg);
+//	char* buffer = new char[length];
+//	
+//	ImportFile.read(buffer, length);
+//	std::string importedFileContent(buffer, length);
+//	delete[] buffer;
+	ImportFile.seekg(0, std::ios::end);
+	std::streamsize length = ImportFile.tellg();
+	ImportFile.seekg(0, std::ios::beg);
 	
-	ImportFile.read(buffer, length);
-	std::string importedFileContent(buffer, length);
-	delete[] buffer;
+	std::string importedFileContent(length, '\0');
 	
+	ImportFile.read(importedFileContent.data(), length);	
+
 	ExportFile << secretFormula(importedFileContent, exportFilename, importFilename);
 	
 	ImportFile.close();
@@ -143,8 +150,9 @@ void createOutOfCommandline(){
 }
 
 std::string secretFormula(std::string &content, std::string exportFilename, std::string importFilename){
-	char keyNum[1];
-	char* keyNumPointer = keyNum;
+	//char keyNum[1];
+	char keyNum;
+	char* keyNumPointer = &keyNum;
 	std::string result;
 	std::string keyFileName;
 	std::cout << "Enter the name of your keyfile: ";
@@ -175,8 +183,10 @@ std::string secretFormula(std::string &content, std::string exportFilename, std:
 			keyFile.clear();
 			keyFile.seekg(0);
 		}
-		keyFile.read(keyNumPointer, 1);
-		temp += static_cast<int>(keyNum[0]);
+		//keyFile.read(keyNumPointer, 1);
+		//temp += static_cast<int>(keyNum[0]);
+		keyFile.read(&keyNum, 1);
+		temp += static_cast<unsigned char>(keyNum);
 		result += std::to_string(temp) + ".";
 	}
 	keyFile.close();
@@ -184,56 +194,135 @@ std::string secretFormula(std::string &content, std::string exportFilename, std:
 	return result;
 };
 
-std::string reverseSecretFormula(std::string &content, std::string exportFilename, std::string importFilename){
-	char keyNum[1];
-	const char* delim = ".";
-	char* keyNumPointer = keyNum;
-	std::string result;
-	std::string keyFileName;
-	std::cout << "Enter the name of your keyfile: ";
-	std::getline(std::cin, keyFileName);
-	
-	std::ifstream keyFile(keyFileName);
-	if(!keyFile.is_open()){
-	std::cout << '\n' << "Error reading key file";
-	return "Error";}
+std::string reverseSecretFormula(std::string &content,
+                                 std::string exportFilename,
+                                 std::string importFilename)
+{
+    std::string result;
+    std::string keyFileName;
 
-	char option;
-	std::cout << "Configuration:" 
-		  << "\n- Importfile: " << importFilename 
-		  << "\n- Exportfile: " << exportFilename 
-		  << "\n- KeyFilename: " << keyFileName 
-		  << "\nContinue with this configuration? (y/N) ";
-	
-	std::cin >> option;
-	
-	if(std::toupper(static_cast<unsigned char>(option)) != 'Y'){
-		std::cout << "***** Aborted process *****\n";
-		keyFile.close();
-		return "";
-	}
+    std::cout << "Enter the name of your keyfile: ";
+    std::getline(std::cin, keyFileName);
 
-	const char* pointa = content.c_str();
-	while(!content.empty()){
-		int charLength = strcspn(pointa, delim);
-		std::string temp1= content.substr(0, charLength);
-		if(temp1.empty() || temp1 == ""){
-		break;
-		}
-		int temp2 = std::stoi(temp1);
-		content.erase(0, charLength+1);
-		if(keyFile.peek() == EOF){
-			keyFile.clear();
-			keyFile.seekg(0);
-		}
-		keyFile.read(keyNumPointer, 1);
-		temp2 -= static_cast<int>(keyNum[0]);
-		result += char(temp2);
-	}
-	keyFile.close();
-	std::cout << "\n*********   Secret uncovered :)   *********" << '\n';
-	return result;
+    std::ifstream keyFile(keyFileName, std::ios::binary);
+
+    if (!keyFile.is_open())
+    {
+        std::cout << "\nError reading key file";
+        return "Error";
+    }
+
+    char option;
+
+    std::cout
+        << "Configuration:"
+        << "\n- Importfile: " << importFilename
+        << "\n- Exportfile: " << exportFilename
+        << "\n- KeyFilename: " << keyFileName
+        << "\nContinue with this configuration? (y/N) ";
+
+    std::cin >> option;
+    std::cin.ignore(2000, '\n');
+
+    if (std::toupper(static_cast<unsigned char>(option)) != 'Y')
+    {
+        std::cout << "***** Aborted process *****\n";
+        return "";
+    }
+
+    while (!content.empty())
+    {
+        size_t pos = content.find('.');
+
+        if (pos == std::string::npos)
+            break;
+
+        std::string token = content.substr(0, pos);
+        content.erase(0, pos + 1);
+
+        if (token.empty())
+            break;
+
+        int value;
+
+        try
+        {
+            value = std::stoi(token);
+        }
+        catch (const std::exception&)
+        {
+            std::cerr << "Invalid token: " << token << '\n';
+            continue;
+        }
+
+        if (keyFile.peek() == EOF)
+        {
+            keyFile.clear();
+            keyFile.seekg(0);
+        }
+
+        char keyByte;
+        keyFile.read(&keyByte, 1);
+
+        value -= static_cast<unsigned char>(keyByte);
+
+        result += static_cast<char>(value);
+    }
+
+    std::cout << "\n*********   Secret uncovered :)   *********\n";
+
+    return result;
 }
+//std::string reverseSecretFormula(std::string &content, std::string exportFilename, std::string importFilename){
+//	char keyNum[1];
+//	const char* delim = ".";
+//	char* keyNumPointer = keyNum;
+//	std::string result;
+//	std::string keyFileName;
+//	std::cout << "Enter the name of your keyfile: ";
+//	std::getline(std::cin, keyFileName);
+//	
+//	std::ifstream keyFile(keyFileName);
+//	if(!keyFile.is_open()){
+//	std::cout << '\n' << "Error reading key file";
+//	return "Error";}
+//
+//	char option;
+//	std::cout << "Configuration:" 
+//		  << "\n- Importfile: " << importFilename 
+//		  << "\n- Exportfile: " << exportFilename 
+//		  << "\n- KeyFilename: " << keyFileName 
+//		  << "\nContinue with this configuration? (y/N) ";
+//	
+//	std::cin >> option;
+//	
+//	if(std::toupper(static_cast<unsigned char>(option)) != 'Y'){
+//		std::cout << "***** Aborted process *****\n";
+//		keyFile.close();
+//		return "";
+//	}
+//
+//	const char* pointa = content.c_str();
+//	while(!content.empty()){
+//		int charLength = strcspn(pointa, delim);
+//		std::string temp1= content.substr(0, charLength)+"\0;
+//		if(temp1.empty() || temp1 == ""){
+//		break;
+//		}
+//		int temp2 = std::stoi(temp1);
+//		content.erase(0, charLength+1);
+//		if(keyFile.peek() == EOF){
+//			keyFile.clear();
+//			keyFile.seekg(0);
+//		}
+//		keyFile.read(keyNumPointer, 1);
+//		temp2 -= static_cast<int>(keyNum[0]);
+//		result += char(temp2);
+//	}
+//	keyFile.close();
+//	std::cout << "\n*********   Secret uncovered :)   *********" << '\n';
+//	return result;
+//}
 
 void uncoverMode(){
 	
@@ -303,15 +392,21 @@ void uncoverOutOfFile(){
 		return;
 	}
 	
-	ImportFile.seekg (0, ImportFile.end);
-    	int length = ImportFile.tellg();
-    	ImportFile.seekg (0, ImportFile.beg);
-	char* buffer = new char[length];
-	
-	ImportFile.read(buffer, length);
-	std::string importedFileContent = buffer;
-	delete[] buffer;
-	
+//	ImportFile.seekg (0, ImportFile.end);
+//    	int length = ImportFile.tellg();
+//    	ImportFile.seekg (0, ImportFile.beg);
+//	char* buffer = new char[length];
+//	
+//	ImportFile.read(buffer, length);
+//	std::string importedFileContent = buffer;
+//	delete[] buffer;
+	ImportFile.seekg(0, std::ios::end);
+	std::streamsize length = ImportFile.tellg();
+	ImportFile.seekg(0, std::ios::beg);
+
+	std::string importedFileContent(length, '\0');
+
+	ImportFile.read(importedFileContent.data(), length);	
 	ExportFile << reverseSecretFormula(importedFileContent, exportFilename, importFilename);
 	
 	ImportFile.close();
